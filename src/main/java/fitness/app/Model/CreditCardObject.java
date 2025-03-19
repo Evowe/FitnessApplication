74e973756ee9ef730308b58006197b8c912a81e1
Evowe <emvowels4@yahoo.com>
2025-03-19 16:24:21 -0500

    Added Maven to project, moved previous files over

---
+++ src/main/java/fitness/app/Model/CreditCardObject.java
@@ -0,0 +1,4 @@
+package fitness.app.Model;
+
+public class CreditCardObject {
+}
---
+++ src/main/java/fitness/app/View/MicroTransactionMain.java
@@ -0,0 +1,12 @@
+
+package fitness.app.View;
+
+
+import javax.swing.*;
+
+public class MicroTransactionMain {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Micro Transaction Interface");
+        TransactionInterface transaction = new TransactionInterface(frame);
+    }
+}
