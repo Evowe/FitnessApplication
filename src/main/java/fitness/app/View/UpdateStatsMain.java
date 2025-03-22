481bb5d8b46557029a6a2e7a4ef8cff15719e582
JambinBen27 <ben_collett1@baylor.edu>
2025-03-22 13:48:15 -0500

    starting point for update sleep

---
+++ src/main/java/fitness/app/View/UpdateStatsMain.java
@@ -0,0 +1,11 @@
+package fitness.app.View;
+
+
+import javax.swing.*;
+
+public class UpdateStatsMain {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("UpdateSleepInterface");
+        UpdateSleepInterface sl = new UpdateSleepInterface(frame);
+    }
+}
