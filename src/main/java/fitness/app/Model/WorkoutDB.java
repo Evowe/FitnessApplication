eba79acb4c153b18ee37640d238812cc58b8ccf2
Anjan G <anjan@Anjans-MacBook-Air.local>
2025-03-23 23:44:34 -0500

    Updated WorkoutDB class with body code

--- src/main/java/fitness/app/Model/WorkoutDB.java
+++ src/main/java/fitness/app/Model/WorkoutDB.java
@@ -1,5 +1,31 @@
 package fitness.app.Model;
 
-public class WorkoutDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class WorkoutDB {
+	void createTable() {
+		//db url
+		String url = "jdbc:sqlite:workouts.db";
+		
+		//create connection with sqlite db
+		try (Connection conn = DriverManager.getConnection(url);
+	             Statement statement = conn.createStatement()) {
+			
+			//creates table for workouts
+			statement.execute(
+					"CREATE TABLE IF NOT EXISTS Books ("
+			                + "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+			                + "Title TEXT NOT NULL,"
+			                + "Author TEXT NOT NULL,"
+			                + "Year INTEGER"
+			                + ");"
+					);
+			
+		} catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+	}
 }
