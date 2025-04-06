9e446b8e045314617067d36d42e8093efdebe065
ngarcia3843 <ngarcia3843@gmail.com>
2025-04-05 23:06:24 -0500

    Completed Calendar Widget

--- src/main/java/fitness/app/Widgets/Calendar/CalendarView.java
+++ src/main/java/fitness/app/Widgets/Calendar/CalendarView.java
@@ -13,7 +13,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 
