dabf58e1fdd051c8131949e0a414b60fe80edbf4
ngarcia3843 <ngarcia3843@gmail.com>
2025-04-09 18:27:09 -0500

    Added goal progress bar

--- src/main/java/fitness/app/Home/HomeView.java
+++ src/main/java/fitness/app/Home/HomeView.java
@@ -2,6 +2,7 @@ package fitness.app.Home;
 
 import fitness.app.Widgets.Battlepass.BattlepassView;
 import fitness.app.Widgets.Calendar.CalendarView;
+import fitness.app.Widgets.GoalProgressMeter.GoalProgressMeterView;
 import fitness.app.Widgets.SideMenu.SideMenuView;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.*;
@@ -15,7 +16,17 @@ public class HomeView extends JPanel {
         setLayout(new MigLayout("insets 20", "left", "top"));
 
         add(new SideMenuView(), "growy, pushy");
-        add(new CalendarView(), "gapx 10");
-        add(new BattlepassView(), "gapx 10, growx, pushx");
+        JPanel whole = new JPanel(new MigLayout("wrap, insets 2"));
+
+        JPanel top = new JPanel(new MigLayout("insets 0", "left", "top"));
+        top.add(new CalendarView(), "gapx 10");
+        top.add(new BattlepassView(), "gapx 10, growx, pushx, wrap");
+        whole.add(top);
+
+        JPanel bottom = new JPanel(new MigLayout("insets 0", "left", "top"));
+        bottom.add(new GoalProgressMeterView(), "gapy 5, gapx 10, growx, pushx, wrap");
+        whole.add(bottom);
+
+        add(whole, "growy, pushy");
     }
 }
