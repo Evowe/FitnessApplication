829707917ffd03c238cc9a1de14125705601101e
JaminBen27 <benecollett@gmail.com>
2025-04-22 13:35:10 -0500

    testing template

---
+++ src/test/java/GoalsModelTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import fitness.app.Goals.GoalsModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GoalsModelTest {
+
+
+    @Test
+    void testVerifyDistance() {
+        String error = GoalsModel.verifyDistance(1.0);
+        if(error != null)
+        {
+            Assertions.fail(error);
+        }
+    }
+
+    @Test
+    void testVerifyDistanceFail() {
+        String error = GoalsModel.verifyDistance(-1.0);
+        if(error == null)
+            Assertions.fail(error);
+    }
+}
